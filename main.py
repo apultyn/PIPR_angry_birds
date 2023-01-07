e625066dbd4c8f7b34fd32a524bbeb522ef033d4
Andrzej Pultyn <andrzej.pultyn.stud@pw.edu.pl>
2023-01-07 15:48:40 +0100

    Upgraded main

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from game import play
+import os
 
 
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+cls()
 work = True
+print('Welcome to Wściekłe Ptaki!')
 while work:
     print("Select level (from 1 to 2)")
+    print("Enter 0 to exit")
     level = int(input())
+    cls()
     if level == 0:
         work = False
     else:
         play(level)
+cls()
