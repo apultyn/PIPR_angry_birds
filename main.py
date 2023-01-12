d8bcd968a7fe4f04b560f71fb41a2b280309742b
Andrzej Pultyn <andrzej.pultyn.stud@pw.edu.pl>
2023-01-12 17:48:35 +0100

    Updated main

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-levels = [0, 1, 2, 3, 4]
+levels = [0, 1, 2, 3, 4, 5, 6, 7]
 
 cls()
 work = True
 print('Welcome to Wściekłe Ptaki!')
 while work:
-    print("Select level (from 1 to 2)")
+    print("Select level (from 1 to 7)")
     print("Enter 0 to exit")
     level = int(input())
     if level == 0:
